c4114bd7f4c6e80c08f2c20ce587acfa698e230d
JoelNickson <joel2000nickson@gmail.com>
2022-02-23 16:43:32 +0530

    add: navigation using arrow keys

--- src/app/question-list/question-list.component.ts
+++ src/app/question-list/question-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { questionsSet, getFromCharCode } from '../questions';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { questionsSet } from '../questions';
 
 @Component({
   selector: 'app-question-list',
@@ -8,10 +8,48 @@ import { questionsSet, getFromCharCode } from '../questions';
 })
 export class QuestionListComponent implements OnInit {
   questionsSet = questionsSet;
-  getFromCharCode = getFromCharCode;
-  constructor() { }
+  index = 0;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
+
+  move(i: number, key: string) {
+    this.index = i;
+    if (key == 'right' || key == 'up') {
+      if (i < questionsSet.length - 1) {
+        document.getElementById(`modal-open-${++this.index}`)?.click();
+        // document.getElementById(`modal-open-${i + 1}`)?.classList.add('.modal-clicked');
+        // console.log("clicked next element");
+      }
+    }
+    if (key == 'left' || key == 'down') {
+      if (i > 0) {
+        document.getElementById(`modal-open-${--this.index}`)?.click();
+        // document.getElementById(`modal-open-${i + 1}`)?.classList.add('.modal-clicked');
+        // console.log("clicked prev element");
+      }
+    }
+    console.log(this.index);
+  }
+  // removeClass() {
+  //   let modal = document.getElementById(`modal-open-${this.index}`)?.classList;
+  //   if (modal?.contains('.modal-clicked')) {
+  //     modal.remove('.modal-clicked');
+  //   }
+  // }
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    switch (event.key) {
+      case "ArrowRight": this.move(this.index, 'right'); break;
+      case "ArrowLeft": this.move(this.index, 'left'); break;
+      case "ArrowUp": this.move(this.index, 'up'); break;
+      case "ArrowDown": this.move(this.index, 'down'); break;
+    }
+  }
+
 
 }
